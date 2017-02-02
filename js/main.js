c2845bebcf3134d0b5ecd0d8550347d84839ddd3
yasyaorel <you@example.com>
2017-02-02 19:17:47 +0200

    Add isotope plagin, add hover effect in footer, tiny changes in form styles and text align un header.

--- js/main.js
+++ js/main.js
@@ -10,4 +10,19 @@ $(function() {
 
     setInterval(change, 3000);
 
+    $('.isotope').isotope({
+        itemSelector: '.isotope__img',
+        layoutMode: 'masonry'
+    });
+
+    $('.filtres__item').click(function() {
+        $('.filtres__item').removeClass('active');
+        $(this).addClass('active');
+
+        var selector = $(this).attr('data-filter');
+        $('.isotope').isotope({
+            filter: selector
+        });
+    });
+
 });
